a6f76715fe8d3a0e3abc144011370c976a9f1886
silveryC <hello.shengC@gmail.com>
2018-08-23 17:01:57 +0800

    .

--- src/main/java/pers/shengc/simpledelaytask/SimpledelaytaskApplication.java
+++ src/main/java/pers/shengc/simpledelaytask/SimpledelaytaskApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author sc
  * @create 2018/8/23
  * @desc 参考：https://mp.weixin.qq.com/s/eDMV25YqCPYjxQG-dvqSqQ?ptlang=2052&ADUIN=1024616676&ADSESSION=1489673030&ADTAG=CLIENT.QQ.5497_.0&ADPUBNO=26661
+ * @desc 参考：https://www.jianshu.com/p/2d2271ecc64d
  **/
 @SpringBootApplication
 public class SimpledelaytaskApplication {
